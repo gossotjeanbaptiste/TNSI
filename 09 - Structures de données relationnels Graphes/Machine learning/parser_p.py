84a538e7857abebfb2d5c767a8ac34bc74717acb
gossotjeanbaptiste <116961839+gossotjeanbaptiste@users.noreply.github.com>
2024-02-16 09:23:42 +0100

    parser fonctionnel

--- 09 - Structures de données relationnels Graphes/Machine learning/parser_p.py
+++ 09 - Structures de données relationnels Graphes/Machine learning/parser_p.py
@@ -1,14 +1,26 @@
-def parser(phrase):
+def analyseur(phrase):
     dico = {}
-    for mot in phrase.split(' '):
-        mot.lower()
-        print(mot)
-        if mot in dico:
-            dico[mot] += 1
+    phrase_splite = phrase.split(' ')
+    n = len(phrase_splite)
+    for indice in range(n-1):
+        mot = phrase_splite[indice].lower().strip('.,')
+        mot_suivant = phrase_splite[indice + 1].lower().strip('.,')
+        if mot not in dico.keys():
+            dico[mot] = {mot_suivant: 1}
         else:
-            dico[mot] = 1
+            if mot_suivant in dico[mot].keys():
+                dico[mot][mot_suivant] += 1
+            else:
+                dico[mot][mot_suivant] = 1
     return dico
 
-test = "Il y a des jours et il y a la journée NSI SNT"
+test = "Il y a des jours et il y a la journée, NSI SNT. Il convient de se préparer pour cette journée pleine d’échanges."
 
-print(parser(test))
+fichier = open("henri.txt", "r", encoding="utf-8")
+lines = fichier.readlines()
+dico = analyseur(test)
+
+for line in lines:
+    analyseur(line.strip('\n'))
+
+print(dico)
