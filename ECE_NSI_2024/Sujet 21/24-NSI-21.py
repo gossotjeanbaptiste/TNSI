2f7644674b9b71ddf0eb6d48260970d86c818f5d
gossotjeanbaptiste <116961839+gossotjeanbaptiste@users.noreply.github.com>
2024-03-15 09:07:04 +0100

    sujet 20 & 21 et descriptif de sujet 8 a 10

--- ECE_NSI_2024/Sujet 21/24-NSI-21.py
+++ ECE_NSI_2024/Sujet 21/24-NSI-21.py
@@ -1,4 +1,18 @@
-def parcours(adj, x, acc):
+# Exercice 1
+
+def recherche_motif(motif, chaine):
+    liste=[]
+    for i in range(len(chaine)):
+        if motif == chaine[i:i+len(motif)]:
+            liste.append(i)
+    return liste
+    
+
+
+# Exercice 2:
+
+
+def parcours(adj, x, acc):  
     '''Réalise un parcours en profondeur récursif
     du graphe donné par les listes d'adjacence adj 
     depuis le sommet x en accumulant les sommets
@@ -6,7 +20,7 @@ def parcours(adj, x, acc):
     if x ...: 
         acc.append(x)
         for y in ...: 
-            parcours(adj, ...) 
+            parcours(adj,...) 
 
 def accessibles(adj, x):
     '''Renvoie la liste des sommets accessibles dans le
@@ -14,6 +28,29 @@ def accessibles(adj, x):
     le sommet x.'''
     acc = []
     parcours(adj, ...) 
+    return acc
+
+
+
+# Correction
+
+
+def parcours(adj, x, acc):  # acc est l'accumulateur
+    '''Réalise un parcours en profondeur récursif
+    du graphe donné par les listes d'adjacence adj 
+    depuis le sommet x en accumulant les sommets
+    rencontrés dans acc'''
+    if x not in acc: 
+        acc.append(x)
+        for y in adj[x]: 
+            parcours(adj, y, acc) 
+
+def accessibles(adj, x):
+    '''Renvoie la liste des sommets accessibles dans le
+    graphe donné par les listes d'adjacence adj depuis
+    le sommet x.'''
+    acc = []
+    parcours(adj, x, acc) 
     return acc
 
 
