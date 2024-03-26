2e4a1ad7ba19f72bba33292e3bb930d9ffb9431e
gossotjeanbaptiste <116961839+gossotjeanbaptiste@users.noreply.github.com>
2024-03-26 11:59:52 +0100

    les algos de chiffrage qui marche (?)

--- 12 - Cryptographie & Sécurisation des communications/crypto_sys.py
+++ 12 - Cryptographie & Sécurisation des communications/crypto_sys.py
@@ -13,7 +13,7 @@ def cesar(chaine: str, decompte: int) -> str:
     return resultat
 
 
-print(cesar("BONJOUR", 54))  # CPOKVPS
+print(cesar("BONJOUR", 5))  # GTSOTZW
 
 
 def dechiffre_cesar(chaine: str, decompte: int) -> str:
@@ -21,12 +21,37 @@ def dechiffre_cesar(chaine: str, decompte: int) -> str:
     return cesar(chaine, -decompte)
 
 
-print(dechiffre_cesar("CPOKVPS", 54))  # BONJOUR
+print(dechiffre_cesar("GTSOTZW", 5))  # BONJOUR
 
 
 def affine(chaine: str, a: int, b: int) -> str:
-    pass
+    # ! Principe Affine
+    # ! On applique une transformation affine à chaque caractère de la chaîne de caractères
+    resultat = ""
+    for lettre in chaine:
+        if 97 <= ord(lettre) <= 122:
+            resultat += chr((a * (ord(lettre) - 97) + b) % 26 + 97)
+        elif 65 <= ord(lettre) <= 90:
+            resultat += chr((a * (ord(lettre) - 65) + b) % 26 + 65)
+        else:
+            resultat += lettre
+    return resultat
 
 
 def vigenere(chaine: str, cle: str) -> str:
-    pass
+    # ! Principe Vigenère
+    # ! On applique un chiffrement de Vigenère à chaque caractère de la chaîne de caractères
+    resultat = ""
+    cle_index = 0
+    for lettre in chaine:
+        if 97 <= ord(lettre) <= 122:
+            decalage = ord(cle[cle_index % len(cle)]) - 97
+            resultat += chr((ord(lettre) - 97 + decalage) % 26 + 97)
+            cle_index += 1
+        elif 65 <= ord(lettre) <= 90:
+            decalage = ord(cle[cle_index % len(cle)]) - 65
+            resultat += chr((ord(lettre) - 65 + decalage) % 26 + 65)
+            cle_index += 1
+        else:
+            resultat += lettre
+    return resultat
