0a41aa3028c492f303dc1e66ad41f7d6f8a84a4e
gossotjeanbaptiste <116961839+gossotjeanbaptiste@users.noreply.github.com>
2024-04-04 16:43:51 +0200

    serveur du discord like

--- 12 - Cryptographie & Sécurisation des communications/projetCrypto/serveur.py
+++ 12 - Cryptographie & Sécurisation des communications/projetCrypto/serveur.py
@@ -8,38 +8,42 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
-client = []
-pseudo = []
+clients = []
+pseudos = []
+
 
 def diffuser(message, client_envoyeur=None):
-    for personne in client: # Parcours de tous les client
-        if personne == client_envoyeur: # Verification, on envoie que si ce n'est pas le client envoyeur
-            client.send(message)
-            
-            
+    for personne in clients:  # Parcours de tous les client
+        if personne == client_envoyeur:  # Verification, on envoie que si ce n'est pas le client envoyeur
+            clients.send(message)
+
+
 def gestion(client):
-    ... : # Boucle infinie pour ecouter en permanence
+    while True:  # Boucle infinie pour ecouter en permanence
         try:
             message = client.recv(1024)
             diffuser(message, client)
             print(message)
         except:
-            index = clients.index(client) # recuperation de l'index du client
-            ... # retirer le client de la liste clients
-            client.close() # deconnexion du client
+            index = clients.index(client)  # recuperation de l'index du client
+            clients.remove(client)  # retirer le client de la liste clients
+            client.close()  # deconnexion du client
             user = pseudos[index]
             diffuser(f'SERVEUR : {user} a quitte le chat'.encode('utf-8'))
-            ... # retirer le pseudo du client de la liste pseudos
+            # retirer le pseudo du client de la liste pseudos
+            pseudos.remove(client)
             break
-        
-        
+
+
 def connexions():
-    ... : # Boucle infinie pour ecouter en permanence
-        client, address = server.accept() # recuperation des informations du client
+    while True:  # Boucle infinie pour ecouter en permanence
+        client, address = server.accept()  # recuperation des informations du client
         print(f'SERVEUR : Connexion etablie avec {str(address)}')
-        pseudo = client.recv(1024).decode('utf-8') # La premiere chose qu'enverra le client est son pseudo, nous pouvons donc traite ce premier messsage
-        ... # ajouter le client et son pseudo aux liste clients et pseudos
-        ...
+        # La premiere chose qu'enverra le client est son pseudo, nous pouvons donc traite ce premier messsage
+        pseudo = client.recv(1024).decode('utf-8')
+        # ajouter le client et son pseudo aux liste clients et pseudos
+        clients.append(client)
+        pseudos.append(pseudo)
 
         print(f'SERVEUR : Nouvel utilisateur {pseudo} connecte')
         diffuser(f'SERVEUR : {pseudo} a rejoint le chat'.encode('utf-8'))
