803c10c8556f759d4779360d2250477f2c4b7377
gossotjeanbaptiste <116961839+gossotjeanbaptiste@users.noreply.github.com>
2024-06-03 09:41:06 +0200

    ??,

--- ECE_NSI_2024/Sujet 26/24-NSI-26.py
+++ ECE_NSI_2024/Sujet 26/24-NSI-26.py
@@ -1,3 +1,24 @@
+#-*- coding: utf-8 -*-
+
+def ajoute_dictionnaires(dico1, dico2):
+    '''Ajoute les valeurs des clés communes de deux dictionnaires.'''
+    dico = {}
+    for cle in dico1:
+        if cle in dico2:
+            dico[cle] = dico1[cle] + dico2[cle]
+        else:
+            dico[cle] = dico1[cle]
+    for cle in dico2:
+        if cle not in dico1:
+            dico[cle] = dico2[cle]
+    return dico
+
+
+print(ajoute_dictionnaires({1: 5, 2: 7}, {2: 9, 3: 11}))
+print(ajoute_dictionnaires({}, {2: 9, 3: 11}))
+print(ajoute_dictionnaires({1: 5, 2: 7}, {}))
+
+
 from random import randint
 
 def nombre_coups():
@@ -9,13 +30,13 @@ def nombre_coups():
     nombre_cases_vues = 1
     cases_vues[0] = True
     case_en_cours = 0
-    n = ... 
-    while ... < ...: 
+    n = 1 
+    while nombre_cases_vues < nombre_cases: 
         x = randint(1, 6)
-        case_en_cours = (case_en_cours + ...) % ... 
-        if ...: 
+        case_en_cours = (case_en_cours + x) % nombre_cases 
+        if not cases_vues[case_en_cours]: 
             cases_vues[case_en_cours] = True
-            nombre_cases_vues = ... 
-        n = ... 
+            nombre_cases_vues = nombre_cases_vues + 1
+        n = n + 1 
     return n
 
