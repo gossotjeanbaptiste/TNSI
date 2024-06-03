83a88fadea4f98eee772fb077148c61cafd39233
gossotjeanbaptiste <116961839+gossotjeanbaptiste@users.noreply.github.com>
2024-06-03 10:37:52 +0200

    ???

--- ECE_NSI_2024/Sujet 26/24-NSI-26.py
+++ ECE_NSI_2024/Sujet 26/24-NSI-26.py
@@ -1,4 +1,7 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+from random import randint
+
 
 def ajoute_dictionnaires(dico1, dico2):
     '''Ajoute les valeurs des clés communes de deux dictionnaires.'''
@@ -14,29 +17,34 @@ def ajoute_dictionnaires(dico1, dico2):
     return dico
 
 
+def version2(d1, d2):
+    return {key: d1.get(key, 0) + d2.get(key, 0) for key in set(d1).union(d2)}
+
+
 print(ajoute_dictionnaires({1: 5, 2: 7}, {2: 9, 3: 11}))
 print(ajoute_dictionnaires({}, {2: 9, 3: 11}))
 print(ajoute_dictionnaires({1: 5, 2: 7}, {}))
 
+print(version2({1: 5, 2: 7}, {2: 9, 3: 11}))
+print(version2({}, {2: 9, 3: 11}))
+print(version2({1: 5, 2: 7}, {}))
 
-from random import randint
 
 def nombre_coups():
     '''Simule un jeu de plateau avec 12 cases et renvoie le nombre
     minimal de coups pour visiter toutes les cases.'''
     nombre_cases = 12
     # indique si une case a été vue
-    cases_vues = [ False ] * nombre_cases
+    cases_vues = [False] * nombre_cases
     nombre_cases_vues = 1
     cases_vues[0] = True
     case_en_cours = 0
-    n = 1 
-    while nombre_cases_vues < nombre_cases: 
+    n = 1
+    while nombre_cases_vues < nombre_cases:
         x = randint(1, 6)
-        case_en_cours = (case_en_cours + x) % nombre_cases 
-        if not cases_vues[case_en_cours]: 
+        case_en_cours = (case_en_cours + x) % nombre_cases
+        if not cases_vues[case_en_cours]:
             cases_vues[case_en_cours] = True
             nombre_cases_vues = nombre_cases_vues + 1
-        n = n + 1 
+        n = n + 1
     return n
-
